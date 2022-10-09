3fd573bd874ed2e50449405f92cc6295cde1b053
ZeeDiaz <93925804+ZeeDiaz@users.noreply.github.com>
2022-10-09 12:51:32 +0800

    Add files via upload

---
+++ Integer Checker.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	
+	int number;
+	
+	printf ("Input number:");
+	scanf ("%d", &number);
+	
+	if (number < 0){
+		
+		printf ("Negative");
+		
+	}
+	
+	else {
+		
+		printf ("Positive");
+		
+	}
+	
+}
